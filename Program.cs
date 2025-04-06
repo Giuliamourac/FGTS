456cad922c94bc9cd457374b5f19aba13d7e8f1b
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-04-06 11:12:54 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            double salario, fgts;
+
+            Console.WriteLine("Digite seu salário para calcular seu FGTS.");
+            salario = Convert.ToDouble(Console.ReadLine()!);
+
+            fgts = (salario / 100) * 8;
+
+            Console.WriteLine($"Salário (R$)..: {salario}");
+            Console.WriteLine($"FGTS: {fgts}");
+        }
+    }
+}
